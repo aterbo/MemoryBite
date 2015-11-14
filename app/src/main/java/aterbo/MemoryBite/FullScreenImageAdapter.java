459903b72716b25ded7d46e071d08ba41e865790
ATerbo <andy.terbovich@gmail.com>
2015-11-14 13:55:48 -0800

    CAPTIONS YES CAPTIONS. Added button to show/hide captions when viewing photos. IT'S STARTING TO BEHAVE LIKE A REAL GODDAMNED PROGRAM.

--- app/src/main/java/aterbo/MemoryBite/FullScreenImageAdapter.java
+++ app/src/main/java/aterbo/MemoryBite/FullScreenImageAdapter.java
@@ -2,16 +2,16 @@ package aterbo.MemoryBite;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -46,6 +46,9 @@ public class FullScreenImageAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         ImageView imgDisplay;
         Button btnClose;
+        final Button btnShowCaption;
+        final TextView caption;
+        int buttonTransparancy = 180;
 
         inflater = (LayoutInflater) activity
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -54,6 +57,7 @@ public class FullScreenImageAdapter extends PagerAdapter {
 
         imgDisplay = (TouchImageView) viewLayout.findViewById(R.id.imgDisplay);
         btnClose = (Button) viewLayout.findViewById(R.id.btnClose);
+        btnClose.getBackground().setAlpha(buttonTransparancy);
 
         Photo photo = photoList.get(position);
 
@@ -62,6 +66,36 @@ public class FullScreenImageAdapter extends PagerAdapter {
 
         //Use UIL with default options presented in GlobalState/Application:
         ImageLoader.getInstance().displayImage(photoPath, imgDisplay);
+
+        //Test if caption present. If so, build caption display
+        if (photo.getPhotoCaption() != null && !photo.getPhotoCaption().isEmpty()){
+            btnShowCaption = (Button) viewLayout.findViewById(R.id.btnShowCaption);
+            btnShowCaption.setVisibility(View.VISIBLE);
+            btnShowCaption.getBackground().setAlpha(buttonTransparancy);
+            btnShowCaption.setText("...");
+
+            caption = (TextView) viewLayout.findViewById(R.id.caption);
+            caption.setText(photo.getPhotoCaption());
+            caption.setMovementMethod(new ScrollingMovementMethod());
+            caption.getBackground().setAlpha(buttonTransparancy);
+
+            // Caption button click event
+            btnShowCaption.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (btnShowCaption.getText() == "...") {
+                        caption.setVisibility(View.VISIBLE);
+                        btnShowCaption.setText(R.string.close_caption);
+                    } else {
+                        caption.setVisibility(View.GONE);
+                        btnShowCaption.setText("...");
+                    }
+
+                }
+            });
+
+
+        }
 
         // close button click event
         btnClose.setOnClickListener(new View.OnClickListener() {
