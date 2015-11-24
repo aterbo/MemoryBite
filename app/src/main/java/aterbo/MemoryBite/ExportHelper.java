aa296d8a4ebad0376c300e830f04be7ac666d7c9
ATerbo <andy.terbovich@gmail.com>
2015-11-24 09:27:02 -0800

    Created ExportHelper class with dialog box to choose export type. Current options are toCSV and Cancel

---
+++ app/src/main/java/aterbo/MemoryBite/ExportHelper.java
@@ -0,0 +1,45 @@
+package aterbo.MemoryBite;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+/**
+ * Created by ATerbo on 11/24/15.
+ */
+public class ExportHelper {
+
+    private Context context;
+
+    public void chooseExportType (Context context){
+
+        this.context = context;
+
+        CharSequence sortOptions[] = new CharSequence[] {
+                context.getResources().getString(R.string.toCSV),
+                context.getResources().getString(R.string.cancel)
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getResources().getString(R.string.export_format));
+        builder.setItems(sortOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        exportToCSV();
+                        break;
+                    case 1:
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void exportToCSV(){
+        Toast.makeText(context, "EXPORT BABY!", Toast.LENGTH_SHORT).show();
+    }
+
+}
