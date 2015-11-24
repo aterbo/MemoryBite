31613d6a542a8d0b9659949531f65577087235b2
ATerbo <andy.terbovich@gmail.com>
2015-11-24 10:30:15 -0800

    Added OpenCSV 3.6.jar to libs folder

--- app/src/main/java/aterbo/MemoryBite/ExportHelper.java
+++ app/src/main/java/aterbo/MemoryBite/ExportHelper.java
@@ -3,7 +3,16 @@ package aterbo.MemoryBite;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  * Created by ATerbo on 11/24/15.
@@ -42,7 +51,36 @@ public class ExportHelper {
     }
 
     private void exportToCSV(){
-        Toast.makeText(context, "EXPORT BABY!", Toast.LENGTH_SHORT).show();
+
+        File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+        if (!exportDir.exists())
+        {
+            exportDir.mkdirs();
+        }
+
+        File file = new File(exportDir, "MemoryBiteCSVExport.csv");
+        try
+        {
+            file.createNewFile();
+            CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+            SQLiteDatabase db = mHelper.getReadableDatabase();
+            Cursor curCSV = db.rawQuery("SELECT * FROM Table",null);
+            csvWrite.writeNext(curCSV.getColumnNames());
+            while(curCSV.moveToNext())
+            {
+                //Which column you want to exprort
+                String arrStr[] ={curCSV.getString(0),curCSV.getString(1), curCSV.getString(2)};
+                csvWrite.writeNext(arrStr);
+            }
+            csvWrite.close();
+            curCSV.close();
+        }
+        catch(Exception sqlEx)
+        {
+            Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+        }
+
+
     }
 
 }
